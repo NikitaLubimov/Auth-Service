21b970eeee307d4a9d28a2af353f55b46b502953
Никита <ikarus11122@gmail.com>
2023-07-10 16:40:35 +0300

    Initial commit

---
+++ src/test/java/ru/gb/fitnessclub/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.fitnessclub.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
