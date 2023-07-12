6f3e9d6d241ed8d78de2727bfbd89616e2bdad2f
Никита <ikarus11122@gmail.com>
2023-07-12 18:55:53 +0300

    v1.1

---
+++ src/main/java/ru/gb/fitnessclub/authservice/service/RoleService.java
@@ -0,0 +1,16 @@
+package ru.gb.fitnessclub.authservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.gb.fitnessclub.authservice.entities.Role;
+import ru.gb.fitnessclub.authservice.repositories.RoleRepository;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+    public Role getUserRole() {
+        return roleRepository.findByName("ROLE_USER").get();
+    }
+}
