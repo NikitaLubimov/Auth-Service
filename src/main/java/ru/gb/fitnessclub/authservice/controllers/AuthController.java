982c7e098af44e90730f9747532d21db73947635
Kirill <kiruxin16@gmail.com>
2023-07-30 16:38:25 +0700

    H2 -> Postgres in properties Сверить логин пароль, с актуальными на сервере. Руками создать пустую бд с именем как в пропетях

--- src/main/java/ru/gb/fitnessclub/authservice/controllers/AuthController.java
+++ src/main/java/ru/gb/fitnessclub/authservice/controllers/AuthController.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gb.fitnessclub.authservice.api.JwtRequest;
 import ru.gb.fitnessclub.authservice.api.RegistrationUserDto;
 import ru.gb.fitnessclub.authservice.service.AuthService;
 
 @RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class AuthController {
 
