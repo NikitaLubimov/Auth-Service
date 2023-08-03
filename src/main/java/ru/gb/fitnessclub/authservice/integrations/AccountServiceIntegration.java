10d31e6a74eb423d93da952623a429b63ec2c559
Никита <ikarus11122@gmail.com>
2023-08-03 23:36:22 +0300

    Добавил обработку исключений.

--- src/main/java/ru/gb/fitnessclub/authservice/integrations/AccountServiceIntegration.java
+++ src/main/java/ru/gb/fitnessclub/authservice/integrations/AccountServiceIntegration.java
@@ -2,11 +2,14 @@ package ru.gb.fitnessclub.authservice.integrations;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import ru.gb.fitnessclub.authservice.api.ClientInfoRequest;
-import ru.gb.fitnessclub.authservice.api.RegistrationUserDto;
+import ru.gb.fitnessclub.authservice.exception.RegistrationException;
+
 
 @Component
 @RequiredArgsConstructor
@@ -20,10 +23,10 @@ public class AccountServiceIntegration {
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(clientInfo)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError,
+                        error -> Mono.error(new RegistrationException("Ошибка создания/обновления аккаунта")))
                 .toBodilessEntity()
                 .block();
 
     }
-
-
 }
